b431cc60e81190c771244972ff103fa603cd204f
ikumen <ikumen@gnoht.com>
2021-06-24 10:45:47 -0700

    Complete remaining api operations (update, get, delete) with additional tests.

--- backend/app/datastores.py
+++ backend/app/datastores.py
@@ -20,25 +20,26 @@ class MongoCollection:
             db = mongo_client.get_database(settings.MONGO_DB_NAME)
         self.collection = db.get_collection(collection_name)
 
-    def create(self, **kwargs):
+    def create(self, **kwargs) -> dict:
         """Add the given attributes to the collection as a new document.
         """
         id = self.collection.insert_one(kwargs).inserted_id
         kwargs['_id'] = id
         return kwargs
 
-    def update(self, id, **kwargs):
+    def update(self, id, **kwargs) -> bool:
         """Update the document in the collection with the given attributes 
-        and identified by the given id.
+        and identified by the given id. Return True if document was updated.
         """
         rv = self.collection.update_one({'_id': objectid.ObjectId(id)}, {'$set': kwargs})
         return rv.matched_count == 1
 
-    def get(self, id: str):
+    def get(self, id: str) -> dict:
         """Return the document in the collection identified by the given id.
         """
-        rv = self.collection.find_one(objectid.ObjectId(id))
-        return rv
+        for rv in self.collection.find({"_id": objectid.ObjectId(id)}).limit(1):
+            return rv
+        return None
 
     def all(self) -> List[dict]:
         """Return all documents in collection.
@@ -46,8 +47,9 @@ class MongoCollection:
         rv = self.collection.find()
         return list(rv)
 
-    def delete(self, id: str):
-        """Delete document in collection identified by the given id.
+    def delete(self, id: str) -> bool:
+        """Delete document in collection identified by the given id. Returns 
+        True if the document was deleted.
         """
         rv = self.collection.delete_one({'_id': objectid.ObjectId(id)})
         return rv.deleted_count == 1
--- backend/app/routes/api.py
+++ backend/app/routes/api.py
@@ -2,30 +2,85 @@ import logging
 
 from starlette.requests import Request
 from starlette.routing import Route
+from starlette.exceptions import HTTPException
 from backend.app.services import note_service
 from backend.app.support import ApiJSONResponse
 
 
 logger = logging.getLogger(__name__)
 
+
+def _log_exception(ex: Exception):
+    logger.warn(f"Caught {type(ex).__name__}. {ex}")
+
+
 # Handler definitions
 async def list_notes(req: Request):
-    logger.info("List all notes")
     notes = note_service.all()
     return ApiJSONResponse(content=notes)
 
 
 async def create_note(req: Request):
-    data = await req.json()
-    logger.info(f"Create a new note: {data}")
-    note = note_service.create(**data)
-    return ApiJSONResponse(content=note)    
+    try:
+        data = await req.json()
+        note = note_service.create(**data)
+        return ApiJSONResponse(content=note)
+    except Exception as ex:
+        _log_exception(ex)
+        raise HTTPException(status_code=400)
+
+
+async def update_note(req: Request):
+    updated_note = None
+    try:
+        id = req.path_params['id']
+        data = await req.json()
+        updated_note = note_service.update(id=id, **data)
+    except Exception as ex:
+        _log_exception(ex)
+        raise HTTPException(status_code=400)
+
+    if updated_note is not None:
+        return ApiJSONResponse(content=updated_note)
+    raise HTTPException(status_code=404)
+
+
+async def get_note(req: Request):
+    note = None
+    try:
+        id = req.path_params['id']
+        note = note_service.get(id)
+    except Exception as ex:
+        _log_exception(ex)
+        raise HTTPException(status_code=400)
+
+    if note is not None:
+        return ApiJSONResponse(content=note)
+    raise HTTPException(status_code=404)
+
+
+async def delete_note(req: Request):
+    is_deleted = False
+    try:
+        id = req.path_params['id']
+        is_deleted = note_service.delete(id)
+    except Exception as ex:
+        _log_exception(ex)
+        raise HTTPException(status_code=400)
+
+    if is_deleted:
+        return ApiJSONResponse(status_code=204)
+    raise HTTPException(status_code=404)
 
 
 # Handler to route mappings
 uri_prefix = '/notes'
+uri_with_id_prefix = uri_prefix + '/{id:str}'
 
 routes = [
     Route(uri_prefix, endpoint=list_notes, methods=['GET']),
-    Route(uri_prefix, endpoint=create_note, methods=['POST'])
+    Route(uri_prefix, endpoint=create_note, methods=['POST']),
+    Route(uri_with_id_prefix, endpoint=get_note, methods=['GET']),
+    Route(uri_with_id_prefix, endpoint=delete_note, methods=['DELETE']),
+    Route(uri_with_id_prefix, endpoint=update_note, methods=['PUT']),
 ]
